6d07c3bf02e65f101c580f031333eee3a05f7376
Marina Fomina <demi87@mail.ru>
2022-03-17 16:55:46 +0700

    add new project

---
+++ src/test/java/CardOrderTest.java
@@ -0,0 +1,25 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class CardOrderTest {
+    private WebDriver driver;
+
+    @BeforeAll
+    public static void setUpAll() {
+        System.setProperty("webdriver.chrome.driver", "./driver/win/chromedriver.exe");
+    }
+
+    @BeforeEach
+    public void setUp () {
+        driver = new ChromeDriver();
+    }
+
+    @AfterEach
+    public void tearDown () {
+        driver.quit();
+        driver = null;
+    }
+}
